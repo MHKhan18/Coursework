86bec98f353330878dd9f39c9bf42bcb7fb13910
Mohammad Khan <mhrkhan13@gmail.com>
2021-11-27 22:09:17 -0500

    use cors

--- CS-554/Lab7/server/app.js
+++ CS-554/Lab7/server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const configRoutes = require('./routes');
+const cors = require('cors')
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 configRoutes(app);
 
