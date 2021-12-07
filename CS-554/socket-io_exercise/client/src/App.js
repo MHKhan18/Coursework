1f0370474bae127ba3b9fee8e7f358d2f34b43ae
Mohammad Khan <mhrkhan13@gmail.com>
2021-12-06 22:40:09 -0500

    complete socket.io exercise

--- CS-554/socket-io_exercise/client/src/App.js
+++ CS-554/socket-io_exercise/client/src/App.js
@@ -6,6 +6,9 @@ function App() {
   const [state, setState] = useState({ message: '', name: '' });
   const [chat, setChat] = useState([]);
 
+  const [rooms, setRooms] = useState(["JavaScript", "Python", "Java"]);
+  const [selectedRoom, setSelectedRoom] = useState("Python");
+
   const socketRef = useRef();
 
   useEffect(() => {
@@ -27,8 +30,8 @@ function App() {
     });
   }, [chat]);
 
-  const userjoin = (name) => {
-    socketRef.current.emit('user_join', name);
+  const userjoin = (name, room) => {
+    socketRef.current.emit('user_join', { name, room });
   };
 
   const onMessageSubmit = (e) => {
@@ -37,13 +40,31 @@ function App() {
     setState({ ...state, [msgEle.name]: msgEle.value });
     socketRef.current.emit('message', {
       name: state.name,
-      message: msgEle.value
+      message: msgEle.value,
+      room: selectedRoom
     });
     e.preventDefault();
     setState({ message: '', name: state.name });
     msgEle.value = '';
     msgEle.focus();
   };
+
+  const onSelectRoom = (e) => {
+    setSelectedRoom(e.target.value);
+    // console.log('Room selected:', e.target.value);
+  }
+
+  const onLeave = () => {
+    socketRef.current.emit('leave', { name: state.name, room: selectedRoom });
+    setState({ message: '', name: '' });
+
+  }
+
+  const onCreateRoom = (e) => {
+    e.preventDefault();
+    setRooms([...rooms, document.getElementById('room_input').value]);
+    document.getElementById('room_input').value = '';
+  }
 
   const renderChat = () => {
     return chat.map(({ name, message }, index) => (
@@ -60,7 +81,7 @@ function App() {
       {state.name && (
         <div className="card">
           <div className="render-chat">
-            <h1>Chat Log</h1>
+            <h1>{`${selectedRoom} Room`}</h1>
             {renderChat()}
           </div>
           <form onSubmit={onMessageSubmit}>
@@ -75,35 +96,63 @@ function App() {
             </div>
             <button>Send Message</button>
           </form>
+          <button onClick={onLeave}>Leave Room</button>
         </div>
       )}
 
       {!state.name && (
-        <form
-          className="form"
-          onSubmit={(e) => {
-            console.log(document.getElementById('username_input').value);
-            e.preventDefault();
-            setState({ name: document.getElementById('username_input').value });
-            userjoin(document.getElementById('username_input').value);
-            // userName.value = '';
-          }}
-        >
-          <div className="form-group">
-            <label>
-              User Name:
+        <div>
+          <form
+            className="form"
+            onSubmit={(e) => {
+              console.log(document.getElementById('username_input').value);
+              e.preventDefault();
+              setState({ name: document.getElementById('username_input').value });
+              userjoin(document.getElementById('username_input').value, selectedRoom);
+              // userName.value = '';
+            }}
+          >
+            <div className="form-group">
+              <label>
+                User Name:
+                <br />
+                <input id="username_input" required />
+              </label>
               <br />
-              <input id="username_input" />
-            </label>
-          </div>
-          <br />
+              <label>
+                Room:
+                <br />
+                <select
+                  value={selectedRoom}
+                  onChange={onSelectRoom}
+                >
+                  {rooms.map((room, index) => {
+                    return (<option value={room}>{room}</option>)
+                  })}
+                </select>
+              </label>
+            </div>
+            <br />
 
-          <br />
-          <br />
-          <button type="submit"> Click to join</button>
-        </form>
-      )}
-    </div>
+            <br />
+            <br />
+            <button type="submit"> Click to join</button>
+          </form>
+
+          <form className="form" onSubmit={(e) => onCreateRoom(e)}>
+            <div className="form-group">
+              <label>
+                New Room:
+                <br />
+                <input id="room_input" required />
+              </label>
+            </div>
+            <button type="submit">Create Room</button>
+          </form>
+        </div >
+      )
+      }
+    </div >
   );
 }
 
--- CS-554/socket-io_exercise/server/app.js
+++ CS-554/socket-io_exercise/server/app.js
@@ -5,17 +5,28 @@ var io = require('socket.io')(http);
 io.on('connection', (socket) => {
   console.log('new client connected', socket.id);
 
-  socket.on('user_join', (name) => {
-    socket.broadcast.emit('user_join', name);
+  socket.on('user_join', ({ name, room }) => {
+    // socket.io room feature
+    socket.join(room);
+
+    // Welcome current user
+    socket.emit('message', { name: 'ChatBot', message: 'Welcome to Mania!' });
+
+    // Let others in the room know
+    socket.broadcast.to(room).emit('user_join', name);
   });
 
-  socket.on('message', ({ name, message }) => {
+  socket.on('message', ({ name, message, room }) => {
     console.log(name, message, socket.id);
-    io.emit('message', { name, message });
+    io.to(room).emit('message', { name, message });
   });
 
   socket.on('disconnect', () => {
     console.log('Disconnect Fired');
+  });
+
+  socket.on('leave', ({ name, room }) => {
+    socket.broadcast.to(room).emit('message', { name: 'ChatBot', message: `${name} has left the room.` });
   });
 });
 
