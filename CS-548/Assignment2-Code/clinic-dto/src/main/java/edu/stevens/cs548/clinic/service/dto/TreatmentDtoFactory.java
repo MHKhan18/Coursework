7f0d9dba3a9fe4d3a297e76878e45280ad4ed2b8
Mohammad Khan <mohammad-khan@Mohammads-MacBook-Pro.local>
2022-09-24 17:42:51 -0400

    add: stubs

---
+++ CS-548/Assignment2-Code/clinic-dto/src/main/java/edu/stevens/cs548/clinic/service/dto/TreatmentDtoFactory.java
@@ -0,0 +1,13 @@
+package edu.stevens.cs548.clinic.service.dto;
+
+public class TreatmentDtoFactory {
+	
+	public DrugTreatmentDto createDrugTreatmentDto () {
+		return new DrugTreatmentDto();
+	}
+	
+	/*
+	 * TODO: Repeat for other treatments.
+	 */
+
+}
