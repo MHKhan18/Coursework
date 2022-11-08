113fac5727206b642022888cbfe05fd9e04913b5
Mohammad Khan <mohammad-khan@Mohammads-MacBook-Pro.local>
2022-11-08 18:06:01 -0500

    add assignment 6 stub

---
+++ CS-548/Assignment6-Code/clinic-domain/src/main/java/edu/stevens/cs548/clinic/domain/PhysiotherapyTreatment.java
@@ -0,0 +1,32 @@
+package edu.stevens.cs548.clinic.domain;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import edu.stevens.cs548.clinic.util.DateUtils;
+
+// TODO
+public class PhysiotherapyTreatment extends Treatment {
+
+	private static final long serialVersionUID = 5602950140629148756L;
+	
+	// TODO (including order by date)
+	protected List<Date> treatmentDates;
+
+	public void addTreatmentDate(LocalDate date) {
+		treatmentDates.add(DateUtils.toDatabaseDate(date));
+	}
+
+	@Override
+	public <T> T export(ITreatmentExporter<T> visitor) {
+            // TODO
+		return null;	
+	}
+	
+	public PhysiotherapyTreatment() {
+		treatmentDates = new ArrayList<>();
+	}
+
+}
