4cd42d8467c86e7fb7b7b1be56d0204ade0355ca
Mohammad Khan <mhrkhan13@gmail.com>
2023-02-20 19:07:54 -0500

    updated stub

---
+++ CS-541/hw1/findFirstWord_modified.py
@@ -0,0 +1,20 @@
+#do not change any function names
+def findAlphabeticallyFirstWord(string):
+    #start your code here
+
+
+
+
+#Ask the user for the input string, separated by a space or comma
+
+    
+
+
+
+
+
+# Find the first word in alphabetical order
+first_word = findAlphabeticallyFirstWord(string)
+
+# Print the result
+
